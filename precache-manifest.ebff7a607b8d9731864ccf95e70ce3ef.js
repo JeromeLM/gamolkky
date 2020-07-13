5d93bbb24b9765be9b3ed906782ba16dc5f9162c
JeromeLM <jerome.lapeyremirande@gmail.com>
2020-07-13 23:19:15 +0200

    Updates

---
+++ precache-manifest.ebff7a607b8d9731864ccf95e70ce3ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4db4df5b5c019ea0d400fbcc2cfe8df",
+    "url": "/gamolkky/index.html"
+  },
+  {
+    "revision": "6881fe5cd97e15bca5af",
+    "url": "/gamolkky/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "de9b8b2c46fa36c6f907",
+    "url": "/gamolkky/static/css/main.184c1946.chunk.css"
+  },
+  {
+    "revision": "6881fe5cd97e15bca5af",
+    "url": "/gamolkky/static/js/2.c932911b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gamolkky/static/js/2.c932911b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9b8b2c46fa36c6f907",
+    "url": "/gamolkky/static/js/main.8ecdce7a.chunk.js"
+  },
+  {
+    "revision": "7d2b6e87111d29e0fb2a",
+    "url": "/gamolkky/static/js/runtime-main.a3caba86.js"
+  },
+  {
+    "revision": "58888783b116b3ccf89a6d4931c65e93",
+    "url": "/gamolkky/static/media/background_image.58888783.jpg"
+  }
+]);
